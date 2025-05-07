82d0a0937014b55b30bbff2d326b27d7a68d8ea7
beratbaru <kagebex@gmail.com>
2025-05-07 13:54:21 +0300

    created sessions table & updated docker compose database credentials

---
+++ blog-api/bootstrap/cache/filament/panels/admin.php
@@ -0,0 +1,91 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'app.filament.resources.category-resource.pages.create-category' => 'App\\Filament\\Resources\\CategoryResource\\Pages\\CreateCategory',
+    'app.filament.resources.category-resource.pages.edit-category' => 'App\\Filament\\Resources\\CategoryResource\\Pages\\EditCategory',
+    'app.filament.resources.category-resource.pages.list-categories' => 'App\\Filament\\Resources\\CategoryResource\\Pages\\ListCategories',
+    'app.filament.resources.comment-resource.pages.create-comment' => 'App\\Filament\\Resources\\CommentResource\\Pages\\CreateComment',
+    'app.filament.resources.comment-resource.pages.edit-comment' => 'App\\Filament\\Resources\\CommentResource\\Pages\\EditComment',
+    'app.filament.resources.comment-resource.pages.list-comments' => 'App\\Filament\\Resources\\CommentResource\\Pages\\ListComments',
+    'app.filament.resources.policy-resource.pages.create-policy' => 'App\\Filament\\Resources\\PolicyResource\\Pages\\CreatePolicy',
+    'app.filament.resources.policy-resource.pages.edit-policy' => 'App\\Filament\\Resources\\PolicyResource\\Pages\\EditPolicy',
+    'app.filament.resources.policy-resource.pages.list-policies' => 'App\\Filament\\Resources\\PolicyResource\\Pages\\ListPolicies',
+    'app.filament.resources.post-resource.pages.create-post' => 'App\\Filament\\Resources\\PostResource\\Pages\\CreatePost',
+    'app.filament.resources.post-resource.pages.edit-post' => 'App\\Filament\\Resources\\PostResource\\Pages\\EditPost',
+    'app.filament.resources.post-resource.pages.list-posts' => 'App\\Filament\\Resources\\PostResource\\Pages\\ListPosts',
+    'app.filament.resources.tag-resource.pages.create-tag' => 'App\\Filament\\Resources\\TagResource\\Pages\\CreateTag',
+    'app.filament.resources.tag-resource.pages.edit-tag' => 'App\\Filament\\Resources\\TagResource\\Pages\\EditTag',
+    'app.filament.resources.tag-resource.pages.list-tags' => 'App\\Filament\\Resources\\TagResource\\Pages\\ListTags',
+    'app.filament.resources.user-resource.pages.create-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\CreateUser',
+    'app.filament.resources.user-resource.pages.edit-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\EditUser',
+    'app.filament.resources.user-resource.pages.list-users' => 'App\\Filament\\Resources\\UserResource\\Pages\\ListUsers',
+    'app.filament.resources.user-resource.pages.view-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\ViewUser',
+    'filament.pages.dashboard' => 'Filament\\Pages\\Dashboard',
+    'filament.widgets.account-widget' => 'Filament\\Widgets\\AccountWidget',
+    'filament.widgets.filament-info-widget' => 'Filament\\Widgets\\FilamentInfoWidget',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.pages.auth.edit-profile' => 'Filament\\Pages\\Auth\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.pages.auth.login' => 'Filament\\Pages\\Auth\\Login',
+    'bezhan-salleh.filament-shield.resources.role-resource.pages.list-roles' => 'BezhanSalleh\\FilamentShield\\Resources\\RoleResource\\Pages\\ListRoles',
+    'bezhan-salleh.filament-shield.resources.role-resource.pages.create-role' => 'BezhanSalleh\\FilamentShield\\Resources\\RoleResource\\Pages\\CreateRole',
+    'bezhan-salleh.filament-shield.resources.role-resource.pages.view-role' => 'BezhanSalleh\\FilamentShield\\Resources\\RoleResource\\Pages\\ViewRole',
+    'bezhan-salleh.filament-shield.resources.role-resource.pages.edit-role' => 'BezhanSalleh\\FilamentShield\\Resources\\RoleResource\\Pages\\EditRole',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    0 => 'Filament\\Pages\\Dashboard',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => '/var/www/html/app/Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+    '/var/www/html/app/Filament/Resources/CategoryResource.php' => 'App\\Filament\\Resources\\CategoryResource',
+    '/var/www/html/app/Filament/Resources/CommentResource.php' => 'App\\Filament\\Resources\\CommentResource',
+    '/var/www/html/app/Filament/Resources/PolicyResource.php' => 'App\\Filament\\Resources\\PolicyResource',
+    '/var/www/html/app/Filament/Resources/PostResource.php' => 'App\\Filament\\Resources\\PostResource',
+    '/var/www/html/app/Filament/Resources/TagResource.php' => 'App\\Filament\\Resources\\TagResource',
+    '/var/www/html/app/Filament/Resources/UserResource.php' => 'App\\Filament\\Resources\\UserResource',
+    0 => 'BezhanSalleh\\FilamentShield\\Resources\\RoleResource',
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => '/var/www/html/app/Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    0 => 'Filament\\Widgets\\AccountWidget',
+    1 => 'Filament\\Widgets\\FilamentInfoWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => '/var/www/html/app/Filament/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Widgets',
+  ),
+);
